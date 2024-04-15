d7fe8205cc74c9eab2563151ba0e69e623e586ea
DianaDzakah <dianadzakah1@gmail.com>
2024-04-15 23:59:12 +0000

    changes

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -8,19 +8,14 @@ const About = () => {
     <section className={styles.body1}>
     <nav className={styles.navbar}>
     <a className={styles.port}>portfolio</a>
-    <div className={styles.nav}>
-    <a>about</a>
-    <a>resume</a>
-    <a>projects</a>
-    <a>contact</a>
-    </div>
     </nav>
     <div className={styles.About}>
     <p className={styles.intro}>Hey I'm Diana Dzakah</p>
     <p className={styles.des}>A Tech Enthusiast, lifelong learner and<br/> emerging life adventurer.</p>
     <img className={styles.image} src={diana} alt='img'/>
+    <p className={styles.title}>Here’s My Story</p>
+    <p className={styles.story}>I'd like to think of it this way, i found love in the most unusual way and place.I originally studied education in the University of Cape Coast graduated with flying colurs.I look forward to what the future holds for i and my love and oops i mean Tech.</p>
     </div>
-
     </section>
     
     </>
--- src/pages/resume/index.jsx
+++ src/pages/resume/index.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
-
+import styles from "./index.module.css"
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <>
+    <section className={styles.resume}>
+    <div className={styles.socials}>
+    <a href='https://www.linkedin.com/in/diana-dzakah-a634442bb?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BgV2blcD9SDyp0v9YbOUciA%3D%3D' className={styles.linkedin}>LinkedIn</a>
+    <a href="https://github.com/DianaDzakah"className={styles.GitHub}>GitHub</a>
+    <a className={styles.resume} href='https://docs.google.com/document/d/1O8i4UGMHEZzurA0FFuek91NLu3Tp3u1k6_zNB7ImBg0/edit' >Resume</a>
+    </div>
+
+    </section>
+   
+    </>
   )
 }
 
