6c189fd9551d43a68315f1f615c623828b84e37b
DianaDzakah <dianadzakah1@gmail.com>
2024-04-16 11:59:09 +0000

    changes

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import styles from "./index.module.css"
 
 const Contact = () => {
   return (
-    <div>Contact</div>
-  )
+    <>
+      <section className={styles.contact}>
+      <div className={styles.con}>
+      <h1>CONTACT ME</h1>
+      <p>Contact me for all your Websites needs</p>
+      <p>dianadzakah1@gmail.com</p>
+      <p>0552231994</p>
+    <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3975.2702325981018!2d-1.7589360258297515!3d4.894361895081476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfe779fefcbdb79b%3A0x3a6e049c74ee0e9e!2sSSNIT%20House!5e0!3m2!1sen!2sgh!4v1701284658784!5m2!1sen!2sgh" width="600" height="450" style={{ border: 0 }} allowFullScreen="" loading="lazy" title="Google Map" referrerPolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      </div>
+      </section>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/pages/projects/index.jsx
+++ src/pages/projects/index.jsx
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from 'react';
+import styles from "./index.module.css";
 
 const Projects = () => {
   return (
     <>
-
+      <section className={styles.body3}>
+        <div className={styles.card}>
+          <img src="https://via.placeholder.com/200x120" alt="Image"/>
+          <div className={styles.cardContent}>
+            <h2>Project 1</h2>
+            <p>Ziibo is a movie straming app </p>
+          </div>
+        </div>
+        
+        <div className={styles.card}>
+          <img src="https://via.placeholder.com/200x120" alt="Image"/>
+          <div className={styles.cardContent}>
+            <h2>Project 2</h2>
+            <p>Marabfoods is an ecommerce site for selling agro products.</p>
+          </div>
+        </div>
+        
+        <div className={styles.card}>
+          <img src="https://via.placeholder.com/200x120" alt="Image"/>
+          <div className={styles.cardContent}>
+            <h2>project 3</h2>
+            <p>This is a group portfolio site </p>
+          </div>
+        </div>
+        
+        <div className={styles.card}>
+          <img src="https://via.placeholder.com/200x120" alt="Image"/>
+          <div className={styles.cardContent}>
+            <h2>Project 4</h2>
+            <p>This is a personal portfolio website.</p>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
